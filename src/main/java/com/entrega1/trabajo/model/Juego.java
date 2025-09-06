492a6fbb681ebb74abfaaafe8b899465358e4b81
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2025-09-05 20:38:48 -0500

    Remover validación maś corrección de encripción

--- src/main/java/com/entrega1/trabajo/model/Juego.java
+++ src/main/java/com/entrega1/trabajo/model/Juego.java
@@ -1,32 +1,32 @@
 package com.entrega1.trabajo.model; //Recuerden cambiar esto
-‎
+
 import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.*;
-import jakarta.validation.constraints.*;
-‎
-‎
+//import jakarta.validation.constraints.*;
+
+
 @Entity
 @Table(name = "Juego")
 public class Juego {
-‎
-    @NotBlank(message = "El nombre es obligatorio")
-    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+
+    //@NotBlank(message = "El nombre es obligatorio")
+    //@Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
     @Column(name = "nombre", nullable = false, length = 100)
     private String nombre;
-‎
+
     //@NotBlank(message = "Cuales son las reglas") Talvez se pueden dejar vacias?
-    @Size(min = 0, max = 1024, message = "El limite es de 1024 characteres")
+    //@Size(min = 0, max = 1024, message = "El limite es de 1024 characteres")
     @Column(name = "reglas", nullable = false, length = 1024)
     private String reglas;
-‎
+
     //Las listas confunden a la base de datos entonces mejor tener un string grande
-    @Size(min = 0, max = 1024, message = "El limite es de 1024 characteres")
+    //@Size(min = 0, max = 1024, message = "El limite es de 1024 characteres")
     @Column(name = "participantes", nullable = false, length = 1024)
     private String participantes;
-‎
-    @NotBlank(message = "la fecha es oligatoria")
+
+    //@NotBlank(message = "la fecha es oligatoria")
     @Column(name = "fecha", nullable = false)
     private Date fecha;
-‎
+
 }
---
+++ src/main/java/com/entrega1/trabajo/model/Scheduler.java
@@ -0,0 +1,21 @@
+package com.entrega1.trabajo.model;//Recuerden cambiar esto
+
+import jakarta.persistence.*;
+
+import java.util.*;
+
+
+
+@Entity
+@Table(name = "Scheduler")
+public class Scheduler {
+
+   @OneToMany(mappedBy = "Shceduler", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Tournament> torneos= new ArrayList<>();
+
+
+   @OneToMany(mappedBy = "juego", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Juego> juegos= new ArrayList<>();
+
+}
+
