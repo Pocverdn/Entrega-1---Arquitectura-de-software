e74c780f64570e8045116f621fc0f0942fba3958
Sebastian Rodriguez <159204182+srodrigub1@users.noreply.github.com>
2025-11-11 04:31:51 -0500

    Update PaymentMethod.java

--- src/main/java/com/entrega1/trabajo/service/payments/PaymentMethod.java
+++ src/main/java/com/entrega1/trabajo/service/payments/PaymentMethod.java
@@ -3,14 +3,15 @@ package com.entrega1.trabajo.service.payments;
 import com.entrega1.trabajo.model.Referee;
 
 /**
- * Interfaz que define el comportamiento genérico para realizar pagos a árbitros.
+ * Interfaz que define el comportamiento genérico para realizar pagos a referees.
  */
 public interface PaymentMethod {
 
     /**
-     * Procesa el pago de un árbitro por el monto indicado.
-     * @param arbitro árbitro que recibirá el pago
-     * @param monto valor del pago
+     * Procesa el pago de un referee por el monto indicado.
+     *
+     * @param referee referee que recibirá el pago
+     * @param monto   valor del pago
      * @return mensaje con el resultado del pago
      */
     String procesarPago(Referee referee, double monto);
